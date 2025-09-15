65d670609e37e434300f73711dda6aba6bf02af0
aayanaqdas <aayan.aqdas@outlook.com>
2025-09-15 21:20:32 +0200

    Initialize server with Express and CORS configuration; add package.json and server.js files

---
+++ server/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+};
+
+const apiKey = process.env.TMDB_API_KEY;
+
+app.use(cors(corsOptions));
+
+
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
